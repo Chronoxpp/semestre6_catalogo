e651f1d8ee5e88e49986112db8e5a75f84c308a6
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2026-08-20 22:02:50 -0400

    feat: adiciona as classes CategoryRepository e CategoryService

---
+++ backend/src/main/java/com/tads20262/catalago/category/CategoryService.java
@@ -0,0 +1,20 @@
+package com.tads20262.catalago.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService
+{
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional
+    public List<Category> findAll()
+    {
+       return repository.findAll();
+    }
+}
